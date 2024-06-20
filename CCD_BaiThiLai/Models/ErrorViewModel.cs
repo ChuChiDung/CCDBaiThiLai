3e5bd18c0753e69e745160a6175cf5b66770256a
ChuChiDung <chuchidung4122002@gmail.com>
2024-06-21 02:05:02 +0700

    first commit

---
+++ CCD_BaiThiLai/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CCD_BaiThiLai.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
